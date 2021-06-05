993c5e2bdf02155ce941c721e7128b740b814804
rloterh <rloterh@gmail.com>
2021-06-05 22:56:53 +0000

    Clean code

--- js/app.js
+++ js/app.js
@@ -1,23 +1,22 @@
-const header = document.querySelector('.header'); 
-const menuItems= document.querySelector('.desktop-nav');
+const header = document.querySelector('.header');
+const menuItems = document.querySelector('.desktop-nav');
 const showMenu = document.querySelector('.menu-bars');
 const closeMenu = document.querySelector('.close-menubars');
 
-
 const displayMenu = () => {
-  showMenu.classList.add('display-none');  
-  closeMenu.classList.add('display-block'); 
-  menuItems.classList.add('display-flex'); 
-  header.classList.add('mobileHeader-height');   
-}
+  showMenu.classList.add('display-none');
+  closeMenu.classList.add('display-block');
+  menuItems.classList.add('display-flex');
+  header.classList.add('mobileHeader-height');
+};
 
 const hideMenu = () => {
-  showMenu.classList.remove('display-none');  
+  showMenu.classList.remove('display-none');
   closeMenu.classList.remove('display-block');
-  menuItems.classList.remove('display-flex'); 
+  menuItems.classList.remove('display-flex');
   header.classList.remove('mobileHeader-height');
   header.classList.add('normalHeader-height');
-}
+};
 
 showMenu.addEventListener('click', displayMenu);
 closeMenu.addEventListener('click', hideMenu);
